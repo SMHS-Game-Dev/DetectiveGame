c44e28f758840f173fa1c47b1618722143febd64
Sparkkilyy <jasonholman47@gmail.com>
2026-03-10 12:42:26 -0500

    Fix previous flawed DIB logic

--- Scripts/KeyManager.gd
+++ Scripts/KeyManager.gd
@@ -1,4 +1,11 @@
 extends Node
 
-var key_prompts: int = 0
 var keys: Array[String] = ["E", "F", "R", "G", "T", "H", "J"]
+var available_keys: Array = range(keys.size())
+
+func claim_key() -> int:
+	return available_keys.pop_front()
+
+func release_key(index: int) -> void:
+	available_keys.append(index)
+	available_keys.sort()
--- Scripts/KeyPromptHandler.gd
+++ Scripts/KeyPromptHandler.gd
@@ -4,6 +4,7 @@ extends Panel
 var location: Vector2
 var can_interact: bool = false
 var key_code: int = 0
+var assigned_key_index: int = -1
 
 func _ready() -> void:
 	_on_resized()
@@ -16,14 +17,14 @@ func _on_resized() -> void:
 func _on_interactable_player_near(state: bool) -> void:
 	can_interact = state
 	if state:
-		key_code = OS.find_keycode_from_string(KeyManager.keys[KeyManager.key_prompts])
-		$Letter.text = KeyManager.keys[KeyManager.key_prompts]
-		KeyManager.key_prompts += 1
-		
+		assigned_key_index = KeyManager.claim_key()
+		key_code = OS.find_keycode_from_string(KeyManager.keys[assigned_key_index])
+		$Letter.text = KeyManager.keys[assigned_key_index]
 		show_key()
 	else:
-		KeyManager.key_prompts -= 1
-	
+		if assigned_key_index != -1:
+			KeyManager.release_key(assigned_key_index)
+			assigned_key_index = -1
 		hide_key()
 
 func show_key() -> void:
